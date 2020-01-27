8fa276496dabe157fc12e17a0add0aeee7994fd4
hchsu (Mac) <hhhhojeihsu@gmail.com>
2020-01-27 20:19:52 +0800

    Partial Rewrite For init.sh

--- init.sh
+++ init.sh
@@ -4,94 +4,45 @@ set -e #stop on error
 
 ans=""
 
-#test if .tmux.conf exsists
-if [ -e ~/.tmux.conf ]; then
-    echo ".tmux.conf already exsists. Do you want to replace it?"
-    read -p "(y/n): " ans
-    if [ ${ans} = "y" ]; then
-        echo "rename old .tmux.conf to .tmux.conf_old?"
-        read -p "(y/n): " ans
-        if [ ${ans} = "y" ]; then
-            mv ~/.tmux.conf ~/.tmux.conf_old
-        else
-            rm ~/.tmux.conf
-        fi
-        cp ./.tmux.conf ~/.tmux.conf
-    elif [ ${ans} = "n" ]; then
-        echo "ignored."
-    else
-        echo "wrong input, ignored."
-    fi
-else
-    echo "copying .tmux.conf"
-    cp ./.tmux.conf ~/.tmux.conf
-fi
+configs=".tmux.conf .bash_profile .vimrc .gitconfig"
 
-
-#test if .bash_profile exsists
-if [ -e ~/.bash_profile ]; then
-    echo ".bash_profile already exsists. Do you want to replace it?"
+for config in ${configs}; do
+  #test if config exsists
+  if [ -f "${HOME}/${config}" ]; then
+    echo "${config} already exsists. Do you want to replace it?"
     read -p "(y/n): " ans
-    if [ ${ans} = "y" ]; then
-        echo "rename old .bash_profile to .bash_profile_old?"
-        read -p "(y/n): " ans
-        if [ ${ans} = "y" ]; then
-            mv ~/.bash_profile ~/.bash_profile_old
-        else
-            rm ~/.bash_profile
-        fi
-        cp ./.bash_profile ~/.bash_profile
-    elif [ ${ans} = "n" ]; then
-        echo "ignored."
+    if [ "${ans}" = "y" ]; then
+      echo "rename old ${config} to ${config}_old?"
+      read -p "(y/n): " ans
+      if [ "${ans}" = "y" ]; then
+        mv -v  "${HOME}/${config}" "${HOME}/${config}_old"
+      fi
+      cp -v "./${config}" ${HOME}/
+    elif [ "${ans}" = "n" ]; then
+      echo "${config} ignored."
     else
-        echo "wrong input, ignored."
+      echo "Wrong input, ignored."
     fi
-else
-    echo "copying .bash_profile"
-    cp ./.bash_profile ~/.bash_profile
-fi
-
-#test if .vimrc exsists
-if [ -e ~/.vimrc ]; then
-    echo ".vimrc already exsists. Do you want to replace it?"
-    read -p "(y/n): " ans
-    if [ ${ans} = "y" ]; then
-        echo "rename old .vimrc to .vimrc_old?"
-        read -p "(y/n): " ans
-        if [ ${ans} = "y" ]; then
-            mv ~/.vimrc ~/.vimrc_old
-        else
-            rm ~/.vimrc
-        fi
-        cp ./.vimrc ~/.vimrc
-    elif [ ${ans} = "n" ]; then
-        echo "ignored."
-    else
-        echo "wrong input, ignored."
-    fi
-else
-    echo "copying .vimrc"
-    cp ./.vimrc ~/.vimrc
-fi
+  else
+    cp -v "./${config}" ${HOME}/
+  fi
+done
 
 #test if Vundle installed
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-
-#test if tpm installed
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 fi
 
 #install vim plugins
 vim +PluginInstall +qall
 
-#source .bash_profile
-source ~/.bash_profile
+#test if tpm installed
+if [ ! -d "${HOME}/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+  #install tmux plugins
+  ${HOME}/.tmux/plugins/tpm/scripts/install_plugins.sh
+fi
 
 #source .tmux.conf
-tmux source ~/.tmux.conf
+tmux source ${HOME}/.tmux.conf
 
-#install tmux plugins
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
